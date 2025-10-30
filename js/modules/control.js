d8a51180631b86dff4c72972ac4215309ceb3cca
Asya Dunaeva <nastassiadunaeva@gmail.com>
2025-10-30 12:42:20 +0300

    feat: add confirm deleting

--- js/modules/control.js
+++ js/modules/control.js
@@ -31,20 +31,23 @@ export const formControl = (form, tbody, key) => {
 export const taskControl = (table, key) => {
     table.addEventListener('click', e => {
         if (e.target.closest('.btn-danger')) {
-            let id;
-            if (e.target.closest('.taskRow') !== null) {
-                id = e.target.closest('.taskRow').children[4].textContent;
-                e.target.closest('.taskRow').remove();
-            }
-            else {
-                id = e.target.closest('.table-success').children[4].textContent;
-                e.target.closest('.table-success').remove();
+            const сonfirmation = confirm("Вы уверены, что хотите удалить эту задачу?");
+            if (сonfirmation) {
+                let id;
+                if (e.target.closest('.taskRow') !== null) {
+                    id = e.target.closest('.taskRow').children[4].textContent;
+                    e.target.closest('.taskRow').remove();
+                }
+                else {
+                    id = e.target.closest('.table-success').children[4].textContent;
+                    e.target.closest('.table-success').remove();
 
+                }
+                removeStorage(key, id);
+                Array.from(table.tbody.children).forEach((el, index) => {
+                    el.children[0].textContent = index + 1;
+                });
             }
-            removeStorage(key, id);
-            Array.from(table.tbody.children).forEach((el, index) => {                
-                el.children[0].textContent = index+1;
-            });            
         } else
             if (e.target.closest('.btn-success')) {
                 e.target.closest('.btn-success').disabled = true;
